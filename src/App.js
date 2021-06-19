4fbab2453b77d89fbbc9592646854e3f1f3b7f52
Cayuela Jean-Loup <jeanloupcayuela30400@gmail.com>
2021-06-19 10:36:32 +0100

    Create rooter , page and remove the useless files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Switch} from "react-router-dom";
+
+//Pages
+import Home from  './Pages/Home'
+import Episode from './Pages/Episode'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/Episode" component={Episode}/>
+    </Switch>
+    </>
   );
 }
 
